04dc8708b80f011b8c7e7b53149cb806347307dc
WillyWilliam00 <133374335+WillyWilliam00@users.noreply.github.com>
2023-09-22 16:20:36 +0000

    up

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const NavCart = document.querySelector(".div-animation")
+const Carts = document.querySelectorAll(".btn-primary:has(.cart-main)")
+const Trash = document.querySelectorAll(".btn-primary:has(.bi-trash3)")
+const SingleCount = document.querySelectorAll(".SingleCount")
+
+
+Carts.forEach( (cartMain, i) => {
+        let count = 0
+        cartMain.addEventListener("click", function() {
+        count++
+        SingleCount[i].innerText = `${count}`
+        NavCart.classList.add("position-relative-animation")
+        cartMain.classList.add("single-book-animation")
+      });
+      
+})
+
+Trash.forEach( SingleTrash => {
+  SingleTrash.addEventListener("click", function() {
+        
+      });
+})
+
+
 
