3a53a66cdccb8f41216f46df218800e50360939f
WillyWilliam00 <133374335+WillyWilliam00@users.noreply.github.com>
2023-09-25 16:29:38 +0000

    up

--- index.js
+++ index.js
@@ -5,20 +5,17 @@ const NavBarList = document.querySelector('.offcanvas-body')
 
 fetch("https://striveschool-api.herokuapp.com/books")
 .then(response => response.json())
-.then(books => {
+.then(books => { console.log(books)
   const ArrayCards = books.map( book => /*html*/
-              `<div class="col-3">
+              `<div class="col-3 my-5">
                 <div class="card">
-                    <img src="${book.img}" class="card-img-top" alt="...">
+                    <img src="${book.img}" class="card-img-top" style="height: 450px" alt="...">
                     <div class="card-body">
-                      <h5 class="card-title">${book.title}</h5>
+                      <h5 class="card-title card-title-inbody">${book.title}</h5>
                       <p class="card-text">${book.price}€</p>
-                      <button type="button" class="btn btn-primary position-relative" onclick='AddBook("${book.price}","${book.title}","${book.img}" )'><span><i class="bi bi-cart cart-main"></i></span>
-                      <div class="number-single-book">
-                        <span class="SingleCount">1</span>
-                      </div>
+                      <button type="button" class="btn btn-primary position-relative" onclick='AddBook("${book.price}", "${book.title}", "${book.img}", event )'><span><i class="bi bi-cart cart-main"></i></span>
                       </button>
-                      <button type="button" class="btn btn-primary" onclick='RemoveBook("${book.title}")'><i class="bi bi-trash3"></i></i></button>
+                      <button type="button" class="btn btn-primary d-none" onclick='RemoveBook("${book.title}", event)'>X TOGLI DAL CARRELLO X</button>
                     </div>
                 </div>
               </div>`
@@ -40,58 +37,82 @@ function text() {
     
   }
   
-function AddBook(price, title, img) {
+function AddBook(price, title, img, event) {
+  const buttonAdd = event.currentTarget.parentElement.querySelector("button:nth-of-type(1)")
+  const buttonRemove = event.currentTarget.parentElement.querySelector("button:nth-of-type(2)")
+  
+  buttonAdd.classList.add("d-none")
+  buttonRemove.classList.remove("d-none")
   CountTot.classList.add("position-relative-animation")
-  const Check = document.querySelectorAll(".ck")
-  Check.forEach((book) => {
-    if(book.innerHTML === title){
-      book.parentElement.parentElement.remove()
-      
-      } 
-  })
+ 
   NavBarList.innerHTML +=  /*html*/
     `<div class=" d-flex flex-row">
     <img style="width: 90px" src="${img}">
     <div class="card-body ms-2">
-      <h5 class="card-title ck">${title}</h5>
-      <p class="card-text">Prezzo: <span class="count">${price}</span></p>
-      <button type="button" class="btn btn-primary" onclick='RemoveBook("${title}")'>X</button>
-      <div class="d-flex">
-        <p class="card-text">Quantità:</p>
-        <input type="number" id="points" name="points">
-      </div>
+      <h5 class="card-title card-title-inchart">${title}</h5>
+      <p class="card-text">Prezzo: <span class="count-inchart">${price}</span></p>
+      <button type="button" class="btn btn-primary" onclick='removefromchart(event, "${title}")'>X</button>
     </div>
     </div>`
     total()
   }
   
-  function RemoveBook(title) {
-    const Check = document.querySelectorAll(".ck")
+  
+  function RemoveBook(title, event) {
+  
+      const buttonAdd = event.currentTarget.parentElement.querySelector("button:nth-of-type(1)")
+      const buttonRemove = event.currentTarget.parentElement.querySelector("button:nth-of-type(2)")
+      const Check = document.querySelectorAll(".card-title-inchart")
+      buttonAdd.classList.remove("d-none")
+      buttonRemove.classList.add("d-none")
+
+    
     Check.forEach((book) => {
-      if(book.innerHTML === title)
-      book.parentElement.parentElement.remove()
+      if(title === book.innerHTML)
+      
+        book.parentElement.parentElement.remove()
     })
     total()
+    
     }
 
+  function removefromchart(event, title) {
+    const Check = document.querySelectorAll(".card-title-inbody")
+    Check.forEach((book) => {
+      if(book.innerHTML === title){
+        const buttonAdd = book.parentElement.querySelector("button:nth-of-type(1)")
+        const buttonRemove = book.parentElement.querySelector("button:nth-of-type(2)")
+        buttonAdd.classList.remove("d-none")
+        buttonRemove.classList.add("d-none")
+      }
+      
+    })
+     event.currentTarget.parentElement.parentElement.remove()
+     total()
+  }
   
+ 
+  function total() {
   
- function total() {
-  let prezzototale = 0
-  const totalPRices = document.querySelectorAll(".count")
-  const totalDOOM = document.querySelector(".total")
-  
-  totalPRices.forEach(x => {
-    
-    prezzototale = prezzototale + parseFloat(x.innerHTML)
-    totalDOOM.innerHTML = prezzototale.toFixed(2)
-    
-  })
+    let prezzototale = 0
+    const totalPrices = document.querySelectorAll(".count-inchart")
+    const totalDOOM = document.querySelectorAll(".total")
+    const quantity = document.querySelector(".quantity")
+    quantity.innerHTML = totalPrices.length
+    totalPrices.forEach(x => {
+      prezzototale = prezzototale + parseFloat(x.innerHTML)
+      
+    })
+    totalDOOM.forEach(total => {
+      total.innerHTML = prezzototale.toFixed(2)
+      })
+
   if(prezzototale === 0){
-    totalDOOM.innerHTML = 0
     CountTot.classList.remove("position-relative-animation")
   }
-  console.log(prezzototale)
+  
+   
+  
  }
 
   
